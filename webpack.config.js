516076f1bede03c210cd097d3fa59eddf41ad93a
Michal Maminski <michal.maminski@gmail.com>
2017-12-10 16:54:20 +0100

    Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist/'),
+        port: 8888
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react', 'stage-2']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Live flickr search build on top of reactjs",
+            template: path.resolve(__dirname, 'src/index-temp.html'),
+            hash: true
+        })
+    ]
+};
